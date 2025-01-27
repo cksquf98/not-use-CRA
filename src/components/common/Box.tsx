918bf563c37408545b011e9c859bb1824486b7d2
chanbyeol <cksquf98@naver.com>
2025-01-27 22:38:36 +0900

    feat: 공통 컴포넌트 분리

---
+++ src/components/common/Box.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Box = styled.div`
+  color: rgba(55, 53, 47, 0.65);
+  background-color: rgba(247, 246, 243);
+  font-size: 16px;
+  padding: 1rem;
+  white-space: pre-wrap; /* 줄바꿈 허용 */
+`;
---
+++ src/components/common/Pre.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Pre = styled.pre`
+  white-space: pre-wrap;
+  background-color: #f4f4f4;
+  padding: 1rem;
+  border-radius: 5px;
+  overflow-x: auto;
+`;
